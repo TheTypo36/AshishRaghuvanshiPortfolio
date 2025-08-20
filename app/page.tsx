8c0c6b7d462bb3c841d6aa2c7bf732727dd2c82f
Ashish Raghuvanshi <thetypo36@gmail.com>
2025-08-21 00:04:22 +0530

    added a heading

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="text-red-500">
-      Ashish Raghuvanshi, Coming soon with portfolio(interactive);
+      <h1>Ashish Raghuvanshi, Coming soon with portfolio(interactive);</h1>
     </div>
   );
 }
